a78d0979ff081ced1af65290523716e2f8d317b2
jimmydin7 <turnspeaker@gmail.com>
2025-09-23 16:43:04 +0300

    creating repo + git ignore

---
+++ utils/get_init_data.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import getpass
+import socket
+
+class MetaData():
+
+    def get_date(self):
+        return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    def get_sys_user(self):
+        return getpass.getuser()
+    
+    def get_sys_machine(self):
+        return socket.gethostname()
+
+    def get_data(self):
+        date = self.get_date()
+        meta_user = self.get_sys_user()
+        meta_machine = self.get_sys_machine()
+        meta = f"{meta_user}@{meta_machine}"
+
+        return date, meta
